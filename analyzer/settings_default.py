8092048b404bb8d27aed7a9c3b90acb5e7a94727
Manuel Reinhardt <manuel.rhdt@gmail.com>
2019-11-09 13:44:23 +0100

    update default settings

--- analyzer/settings_default.py
+++ analyzer/settings_default.py
@@ -1,23 +1,19 @@
-configuration = {
-    # Path to the Gillespie executable
-    'executable': '../CPP/build/Gillespie',
-    'gillespie_cwd': '../runs/response'
-}
+configuration = {}
 
 configuration['mutual_information'] = {
     'input_name': 'response.inp',
-    'signals': '../data/signal/sig{sig}.traj',
-    'responses': '../data/res{sig}-{res}.traj',
-    'num_signals': 30,
-    'num_responses': 30,
+    'signals': '/data/signal/sig{sig}.traj',
+    'responses': '/dev/shm/res{sig}-{res}.traj',
+    'num_signals': 5000,
+    'num_responses': 100,
 
     # there will be (signal_duration * signal_resolution) timestamps in the signal
-    'signal_duration': 3000,  # in seconds
+    'signal_duration': 2500,  # in seconds
     'signal_resolution': 100,  # in 1/second
 
     'signal_component_name': 'S',
     'signal_mean': 4000,
-    'signal_correlation_time': 100,
-    'signal_diffusion': 40,
-    'output': '../data/run0/mi'
+    'signal_correlation_time': 200,
+    'signal_diffusion': 20,
+    'output': '$HOME/run10/mi'
 }
