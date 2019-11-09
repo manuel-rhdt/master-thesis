5324f0ce1c91dc5ed6e52d29720e6147450f39fe
Manuel Reinhardt <manuel.rhdt@gmail.com>
2019-11-09 16:51:52 +0100

    Improve code quality of stochastic sim

--- analyzer/analyzer/stochastic_sim.py
+++ analyzer/analyzer/stochastic_sim.py
@@ -17,30 +17,26 @@ def calc_propensities(components, reaction_k, reaction_reactants, propensities):
 
 
 @njit(fastmath=True)
-def try_propagate_time(progress, random_variates, timestamps, next_ext_timestamp, components, reaction_k, reaction_reactants, propensities):
-    """ Returns `True` if time was successfully propagated. 
+def try_propagate_time(random_variate, timestamp, next_ext_timestamp, total_propensity):
+    """ Returns `True` if a reaction should be executed before continuing. 
 
-    If successfull the new timestamp will be saved in `timestamps`.
+    The second return value is the amount of time that was propagated.
+
+    The random variate should be the negative log of a uniform value sampled from the interval [0.0, 1.0).
     """
-    max_time_step = next_ext_timestamp - timestamps[progress]
+    max_time_step = next_ext_timestamp - timestamp
 
     if max_time_step <= 0.0:
-        return False
-
-    calc_propensities(components, reaction_k, reaction_reactants, propensities)
-    total_propensity = numpy.sum(propensities)
+        return (False, 0.0)
 
     assert total_propensity >= 0.0
 
-    if max_time_step * total_propensity < random_variates[progress]:
-        random_variates[progress] -= max_time_step * total_propensity
-        timestamps[progress + 1] += max_time_step
+    if max_time_step * total_propensity < random_variate:
         # no progress was made
-        return False
+        return (False, max_time_step)
     else:
-        time_step = random_variates[progress] / total_propensity
-        timestamps[progress + 1] += time_step
-        return True
+        time_step = random_variate / total_propensity
+        return (True, time_step)
 
 
 @njit(fastmath=True)
@@ -63,10 +59,10 @@ def select_reaction(propensities):
 def update_components(selected_reaction, components, reaction_reactants, reaction_products):
     for reactant in reaction_reactants[selected_reaction]:
         if reactant >= 0:
-            components[reactant] -= 1.0
+            components[reactant] -= 1
     for product in reaction_products[selected_reaction]:
         if product >= 0:
-            components[product] += 1.0
+            components[product] += 1
 
 
 @njit(fastmath=True)
@@ -98,21 +94,22 @@ def simulate_one(timestamps, trajectory, ext_components, ext_timestamps, reactio
     num_ext_comps = len(ext_components) if ext_components is not None else 0
     ext_length = len(ext_timestamps) if ext_timestamps is not None else 0
 
+    # set the trajectory's initial values
+    timestamps[0] = 0.0
+    trajectory[:, progress] = components[num_ext_comps:]
+
+    # define values used during iteration
     progress = 0
     ext_progress = 0
     components = numpy.zeros(num_comps + num_ext_comps)
-
-    # use the initial values
-    timestamps[:] = 0.0
     for i in range(num_ext_comps):
         if ext_components is not None:
             components[i] = ext_components[i][ext_progress]
     for comp in range(num_comps):
         components[comp + num_ext_comps] = trajectory[comp][progress]
-
-    trajectory[:, progress] = components[num_ext_comps:]
-
     propensities = numpy.zeros(reaction_k.shape)
+    accumulated_time = 0.0
+    current_time = 0.0
 
     while progress + 1 < length:
         if ext_progress >= ext_length:
@@ -120,21 +117,28 @@ def simulate_one(timestamps, trajectory, ext_components, ext_timestamps, reactio
         elif ext_timestamps is not None:
             next_ext_timestamp = ext_timestamps[ext_progress]
 
-        if try_propagate_time(progress, random_variates, timestamps, next_ext_timestamp,
-                              components, reaction_k, reaction_reactants, propensities):
-            progress += 1
+        calc_propensities(components, reaction_k,
+                          reaction_reactants, propensities)
+        total_propensity = numpy.sum(propensities)
 
+        perform_reaction, timestep = try_propagate_time(
+            random_variates[progress], current_time, next_ext_timestamp, total_propensity)
+        
+        current_time += timestep
+
+        if perform_reaction:
             # Update components
             selected_reaction = select_reaction(propensities)
-            reaction_events[progress - 1] = selected_reaction
             update_components(selected_reaction, components,
                               reaction_reactants, reaction_products)
 
+            # update trajectory
+            progress += 1
+            timestamps[progress] = current_time
+            reaction_events[progress - 1] = selected_reaction
             trajectory[:, progress] = components[num_ext_comps:]
-
-            if progress + 1 < length:
-                timestamps[progress + 1] = timestamps[progress]
         else:
+            random_variates[progress] -= timestep * total_propensity
             # update the external trajectory
             ext_progress += 1
             for i in range(num_ext_comps):
