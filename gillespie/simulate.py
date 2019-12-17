772eacceacc07b1e744135948fde6128b130dd8b
Manuel Reinhardt <M.Reinhardt@amolf.nl>
2019-12-17 12:22:50 +0100

    wip

--- gillespie/simulate.py
+++ gillespie/simulate.py
@@ -3,11 +3,19 @@ import numpy
 from . import stochastic_sim
 
 
-def simulate(length, reactions, ext_timestamps=None, ext_components=None):
+def simulate(length, reactions, ext_trajectory=None):
     timestamps = numpy.zeros(length, dtype=numpy.double)
     trajectory = numpy.zeros((1, length), dtype=numpy.uint16)
     reaction_events = numpy.zeros(length - 1, dtype=numpy.uint8)
     network = stochastic_sim.create_reaction_network(**reactions)
+
+    if ext_trajectory is not None:
+        ext_timestamps = ext_trajectory['timestamps']
+        ext_components = ext_trajectory['components']
+    else:
+        ext_timestamps = None
+        ext_components = None
+
     stochastic_sim.simulate_one(
         timestamps, trajectory, reaction_events, network, ext_timestamps, ext_components
     )
