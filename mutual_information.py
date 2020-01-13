caae94b22b289c097467ffc5a6fa445542de0d19
Manuel Reinhardt <M.Reinhardt@amolf.nl>
2020-01-13 23:21:53 +0100

    wip

--- mutual_information.py
+++ mutual_information.py
@@ -13,7 +13,7 @@ from scipy.special import logsumexp
 
 import gillespie.configuration
 from gillespie import trajectory_array as ta
-from gillespie.likelihood import log_p, log_p_multi
+from gillespie.likelihood import log_p, log_p_average
 from gillespie.simulate import simulate
 
 
@@ -51,18 +51,15 @@ class Run:
 
 
 def conditional_likelihood(traj_lengths, signal, response, network):
-    # result = []
+    result = []
     sig = signal.to_delayed()
-    # assert len(sig) == 1
-    # for res, chunk in zip(response.to_delayed(), response.chunks[0]):
-    #     ll = delayed(log_p)(traj_lengths, sig[0], res, network)
-    #     result.append(
-    #         da.from_delayed(ll, shape=(chunk,) + traj_lengths.shape, dtype=np.double)
-    #     )
-
-    # return da.concatenate(result, axis=0)
-    ll = delayed(log_p)(traj_lengths, sig[0], response.to_delayed()[0], network)
-    return da.from_delayed(ll, shape=(response.chunks[0][0], response.chunks[1][0]), dtype=np.double)
+    assert len(sig) == 1
+    for res, chunk in zip(response.to_delayed(), response.chunks[0]):
+        ll = delayed(log_p)(traj_lengths, sig[0], res, network)
+        result.append(
+            da.from_delayed(ll, shape=(chunk,) + traj_lengths.shape, dtype=np.double)
+        )
+    return da.concatenate(result, axis=0)
 
 
 def marginal_likelihood(traj_lengths, signal, response, network):
@@ -70,16 +67,14 @@ def marginal_likelihood(traj_lengths, signal, response, network):
 
     for res, res_chunk in zip(response.to_delayed(), response.chunks[0]):
         level2 = []
-        for sig, sig_chunk in zip(signal.to_delayed(), signal.chunks[0]):
-            ll = delayed(log_p_multi)(traj_lengths, sig, res, network)
+        for sig in signal.to_delayed():
+            ll = delayed(log_p_average)(traj_lengths, sig, res, network)
             level2.append(
                 da.from_delayed(
-                    ll,
-                    shape=(res_chunk, sig_chunk, response.chunks[1][0]),
-                    dtype=np.double,
+                    ll, shape=(res_chunk,) + traj_lengths.shape, dtype=np.double
                 )
             )
-        level1.append(da.concatenate(level2, axis=1))
+        level1.append(da.stack(level2, axis=1))
 
     return da.concatenate(level1, axis=0)
 
@@ -100,13 +95,14 @@ def simulate_batched(count, batch, length, network, ext_trajectory=None):
     return ta.concatenate(trajectories)
 
 
-def simulate_outer(count, length, network, ext_trajectory):
+def simulate_outer(count, batch, length, network, ext_trajectory):
     r = np.empty(len(ext_trajectory), dtype=object)
     for i, s in enumerate(ext_trajectory):
-        response = delayed(simulate)(
-            count, length, network, ext_trajectory=s, initial_value=50
-        )
-        r[i] = ta.from_delayed(response, count, length)
+        r[i] = simulate_batched(count, batch, length, network, ext_trajectory=s)
+        # response = delayed(simulate)(
+        #     count, length, network, ext_trajectory=s, initial_value=50
+        # )
+        # r[i] = ta.from_delayed(response, count, length)
     return r
 
 
@@ -133,8 +129,8 @@ def computation_start(conf, path):
         print("Connecting to", cluster.dashboard_link)
     client = Client(cluster)
     print(client)
-    # import dask
-    # dask.config.set(scheduler='threads')
+    #     import dask
+    #     dask.config.set(scheduler='threads')
     sig_network, res_network = gillespie.configuration.read_reactions(conf)
 
     length = conf["length"]
@@ -145,7 +141,9 @@ def computation_start(conf, path):
     batch = conf["batch_size"]
 
     ce_signals = simulate_batched(ce_num_signals, batch, length, sig_network)
-    ce_responses = simulate_outer(ce_res_per_signal, length, res_network, ce_signals)
+    ce_responses = simulate_outer(
+        ce_res_per_signal, batch*4, length, res_network, ce_signals
+    )
 
     me_responses = signals_and_responses(
         me_num_responses, batch, length, sig_network, res_network
@@ -158,21 +156,23 @@ def computation_start(conf, path):
     ce = []
     for s, r in zip(ce_signals, ce_responses):
         ce.append(-conditional_likelihood(traj_lengths, s, r, res_network))
-    ce = da.stack(ce).mean(axis=1).compute()
+    ce = da.stack(ce).mean(axis=1).persist()
 
-    me = marginal_likelihood(traj_lengths, me_signals, me_responses, res_network)
-    me = -da.reduction(
+    me = -marginal_likelihood(
+        traj_lengths, me_signals, me_responses, res_network
+    ).persist()
+
+    me = da.reduction(
         me, chunk=logsumexp, aggregate=logsumexp, axis=1, dtype=np.double
     ) + np.log(me.shape[1])
-    me = me.compute()
 
     mi = ce.mean(axis=0) - me.mean(axis=0)
     np.savez(
         path,
         trajectory_length=traj_lengths,
-        marginal_entropy=me,
-        conditional_entropy=ce,
-        mutual_information=mi,
+        marginal_entropy=me.compute(),
+        conditional_entropy=ce.compute(),
+        mutual_information=mi.compute(),
     )
 
 
