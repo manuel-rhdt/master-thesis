884d98f084031db1dcccd61aedaa4eac010bb10c
Manuel Reinhardt <manuel.rhdt@gmail.com>
2020-01-18 20:57:19 +0100

    refactor and allow cmd argument

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,23 @@
+mod configuration;
 mod gillespie;
 mod likelihood;
 
-use std::collections::hash_map::DefaultHasher;
-use std::collections::{HashMap, HashSet};
+use std::env;
 use std::fs::{self, File};
-use std::hash::{Hash, Hasher};
 use std::io::prelude::*;
-use std::path::PathBuf;
 
-use gillespie::{ReactionNetwork, SimulationCoordinator, Trajectory, TrajectoryIterator};
+use configuration::{calculate_hash, create_dir_if_not_exists, Config};
+use gillespie::{SimulationCoordinator, Trajectory, TrajectoryIterator};
 use likelihood::log_likelihood;
 
 use ndarray::{Array, Array1, Array2, ArrayView1, Axis};
 use ndarray_npy::WriteNpyExt;
-use rand::{self, SeedableRng};
-use rand_chacha::ChaChaRng;
 use rayon;
 use rayon::prelude::*;
-use serde::Deserialize;
 use toml;
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-fn calculate_hash<T: Hash + ?Sized>(t: &T) -> u64 {
-    let mut s = DefaultHasher::new();
-    t.hash(&mut s);
-    s.finish()
-}
 
 fn conditional_likelihood(
     traj_lengths: &[f64],
@@ -84,175 +74,38 @@ pub fn log_mean_exp(values: ArrayView1<f64>) -> f64 {
         + max
 }
 
-#[derive(Deserialize, Debug, Clone, PartialEq)]
-struct Config {
-    output: PathBuf,
-    batch_size: usize,
-    length: f64,
-    conditional_entropy: ConfigConditionalEntropy,
-    marginal_entropy: ConfigMarginalEntropy,
-    signal: ConfigReactionNetwork,
-    response: ConfigReactionNetwork,
-}
-
-impl Config {
-    pub fn hash_relevant<H: Hasher>(&self, hasher: &mut H) {
-        self.conditional_entropy.hash(hasher);
-        self.marginal_entropy.hash(hasher);
-        self.length.to_bits().hash(hasher);
-        self.signal.hash(hasher);
-        self.response.hash(hasher);
-    }
-
-    pub fn get_relevant_hash(&self) -> u64 {
-        let mut s = DefaultHasher::new();
-        self.hash_relevant(&mut s);
-        s.finish()
-    }
-
-    pub fn create_coordinator(&self, seed: u64) -> SimulationCoordinator<ChaChaRng> {
-        let rng = ChaChaRng::seed_from_u64(seed);
-        SimulationCoordinator {
-            trajectory_len: self.length,
-
-            sig_network: self.signal.to_reaction_network(),
-            res_network: self.response.to_reaction_network(),
-
-            rng,
-        }
-    }
-}
-
-#[derive(Deserialize, Debug, Copy, Clone, PartialEq, Eq, Hash)]
-struct ConfigConditionalEntropy {
-    num_signals: usize,
-    responses_per_signal: usize,
-}
-
-#[derive(Deserialize, Debug, Copy, Clone, PartialEq, Eq, Hash)]
-struct ConfigMarginalEntropy {
-    num_signals: usize,
-    num_responses: usize,
-}
-
-#[derive(Deserialize, Debug, Clone)]
-struct ConfigReactionNetwork {
-    initial: f64,
-    components: Vec<String>,
-    reactions: Vec<Reaction>,
-}
-
-impl PartialEq for ConfigReactionNetwork {
-    fn eq(&self, right: &ConfigReactionNetwork) -> bool {
-        self.initial.to_bits() == right.initial.to_bits()
-            && self.components == right.components
-            && self.reactions == right.reactions
-    }
-}
-
-impl Hash for ConfigReactionNetwork {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.initial.to_bits().hash(state);
-        self.components.hash(state);
-        self.reactions.hash(state);
-    }
-}
-
-impl ConfigReactionNetwork {
-    fn to_reaction_network(&self) -> ReactionNetwork {
-        let components: HashSet<_> = self.components.iter().collect();
-        let mut external_components = HashSet::new();
-        for reaction in &self.reactions {
-            for name in reaction.reactants.iter().chain(reaction.products.iter()) {
-                if !components.contains(name) {
-                    external_components.insert(name);
-                }
-            }
-        }
-        let num_ext_components = external_components.len();
-
-        let mut name_table = HashMap::new();
-        name_table.extend(
-            external_components
-                .iter()
-                .enumerate()
-                .map(|(value, &key)| (key, value)),
-        );
-        name_table.extend(
-            components
-                .iter()
-                .enumerate()
-                .map(|(value, &key)| (key, value + num_ext_components)),
-        );
-
-        let mut k = Vec::with_capacity(self.reactions.len());
-        let mut reactants = Vec::with_capacity(self.reactions.len());
-        let mut products = Vec::with_capacity(self.reactions.len());
-
-        for reaction in &self.reactions {
-            k.push(reaction.k);
-            let r = reaction
-                .reactants
-                .iter()
-                .map(|reactant| name_table[reactant] as u32)
-                .collect();
-            reactants.push(r);
-            let p = reaction
-                .products
-                .iter()
-                .map(|product| name_table[product] as u32)
-                .collect();
-            products.push(p);
-        }
-
-        ReactionNetwork {
-            k,
-            reactants,
-            products,
-        }
-    }
-}
-
-#[derive(Deserialize, Debug, Clone)]
-struct Reaction {
-    k: f64,
-    reactants: Vec<String>,
-    products: Vec<String>,
-}
-
-impl PartialEq for Reaction {
-    fn eq(&self, right: &Reaction) -> bool {
-        self.k.to_bits() == right.k.to_bits()
-            && self.reactants == right.reactants
-            && self.products == right.products
-    }
-}
-
-impl Hash for Reaction {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.k.to_bits().hash(state);
-        self.reactants.hash(state);
-        self.products.hash(state);
-    }
-}
-
 enum EntropyType {
     Conditional,
     Marginal,
 }
 
-fn create_dir_if_not_exists<P: AsRef<std::path::Path>>(path: P) -> std::io::Result<()> {
-    match fs::create_dir(path) {
-        Ok(()) => Ok(()),
-        Err(err) if err.kind() == std::io::ErrorKind::AlreadyExists => Ok(()),
-        other => other,
-    }
+fn print_help() -> ! {
+    println!("\
+Usage:
+    gillespie [conf]
+
+Arguments:
+    conf - The path to the configuration file. The default is 
+           'configuration.toml'.
+");
+    std::process::exit(0)
 }
 
 fn main() -> std::io::Result<()> {
     use std::os::unix::fs::OpenOptionsExt;
 
-    let mut config_file = File::open("configuration.toml")?;
+    let args: Vec<String> = env::args().collect();
+    let configuration_filename = match args.len() {
+        // no arguments passed
+        1 => "configuration.toml",
+        // one argument passed
+        2 => &args[1],
+        _ => {
+            print_help()
+        }
+    };
+
+    let mut config_file = File::open(configuration_filename)?;
     let mut contents = String::new();
     config_file.read_to_string(&mut contents)?;
     let conf: Config = toml::from_str(&contents)?;
